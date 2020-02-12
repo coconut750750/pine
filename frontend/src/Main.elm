682b8f36fd22ced28425b91fba8b95c9b82e25e5
Scott Kim <scottk3@illinois.edu>
2020-02-11 20:42:21 -0600

    Fix frontend to make Element.layout top-level and center for aesthetics

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,37 +1,38 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text, input)
-import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (..)
-import Element exposing (Element, el, row, alignRight, fill, width, rgb255, spacing, centerY, padding)
+import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
+import Html exposing (Html)
+import Html.Events exposing (onClick, onInput)
+
+
 
 -- MAIN
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 
 -- MODEL
 
 
-type alias Model = {
-    content : String,
-    count: Int
-  }
+type alias Model =
+    { content : String
+    , count : Int
+    }
 
 
 init : Model
-init = {
-    content = "",
-    count = 1
-  }
+init =
+    { content = ""
+    , count = 1
+    }
 
 
 
@@ -39,22 +40,22 @@ init = {
 
 
 type Msg
-  = Increment
-  | Decrement
-  | TextUpdate String
+    = Increment
+    | Decrement
+    | TextUpdate String
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      { model | count = model.count + 1 }
+    case msg of
+        Increment ->
+            { model | count = model.count + 1 }
 
-    Decrement ->
-      { model | count = model.count - 1 }
+        Decrement ->
+            { model | count = model.count - 1 }
 
-    TextUpdate newcontent ->
-      { model | content = newcontent }
+        TextUpdate newcontent ->
+            { model | content = newcontent }
 
 
 
@@ -63,33 +64,35 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ Element.layout [] (myInput model.content)
-    , Element.layout [] (myCounter model.count)
-    ]
+    Element.layout
+        [ width fill, height fill ]
+        (column [ centerX, centerY ] [ myInput model.content, myCounter model.count ])
+
 
 myCounter : Int -> Element Msg
-myCounter count = 
-  row []
-    [ Input.button [ padding 30 ]
-        { onPress = Just Decrement
-        , label = Element.text "-"
-        }
-    , el
-        [ Border.rounded 3
-        , padding 30
+myCounter count =
+    row []
+        [ Input.button [ padding 30 ]
+            { onPress = Just Decrement
+            , label = Element.text "-"
+            }
+        , el
+            [ Border.rounded 3
+            , padding 30
+            ]
+            (Element.text (String.fromInt count))
+        , Input.button [ padding 30 ]
+            { onPress = Just Increment
+            , label = Element.text "+"
+            }
         ]
-        (Element.text (String.fromInt count))
-    , Input.button [ padding 30 ]
-        { onPress = Just Increment
-        , label = Element.text "+"
-        }
-    ]
+
 
 myInput : String -> Element Msg
-myInput content = Input.text []
-  { onChange = TextUpdate 
-  , text = content
-  , placeholder = Just (Input.placeholder [] (Element.text "enter some text"))
-  , label = Input.labelAbove [] (Element.text "")
-  }
+myInput content =
+    Input.text []
+        { onChange = TextUpdate
+        , text = content
+        , placeholder = Just (Input.placeholder [] (Element.text "enter some text"))
+        , label = Input.labelAbove [] (Element.text "")
+        }

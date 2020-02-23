b1eb73c606dcad1e21eaa57cabb8a9d1f62aa02a
Scott Kim <scottk3@illinois.edu>
2020-02-23 14:51:57 -0600

    Haskell Interpreting Works

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -3,26 +3,27 @@ module Main exposing (..)
 import Browser
 import Css exposing (..)
 import Css.Global exposing (..)
+import Debug exposing (log)
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
-import Element.Events as Event exposing (on)
 import Element.Font as Font
 import Element.Input as Input
 import Generated.Decoder exposing (decodeCodeSubmission)
 import Generated.Encoder exposing (encodeCodeSubmission)
 import Generated.Types exposing (CodeSubmission)
-import Html exposing (Html, div, textarea, input, text)
+import Html exposing (Html, div, input, text, textarea)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick, onInput, preventDefaultOn)
+import Html.Styled
 import Html.Styled.Attributes exposing (css)
 import Html.Styled.Events
-import Html.Styled
 import Http exposing (stringBody)
-import Json.Encode
 import Json.Decode as Decode
-import Debug exposing (log)
+import Json.Encode
 import Utils.CssUtils as CssUtils
+
+
 
 -- MAIN
 
@@ -109,9 +110,12 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
+
+
 -- Style
 
-theme : { primary : Css.Color, secondary : Css.Color, border: Css.Color, text: Css.Color }
+
+theme : { primary : Css.Color, secondary : Css.Color, border : Css.Color, text : Css.Color }
 theme =
     { primary = Css.hex "20252d"
     , secondary = Css.hex "1c2027"
@@ -120,131 +124,169 @@ theme =
     }
 
 
+
 -- VIEW
+
 
 view : Model -> Html.Styled.Html Msg
 view model =
-  let
-    headerHeight = Css.vh 10
-    replHeight = Css.calc (Css.vh 100) Css.minus headerHeight
-  in
-  Html.Styled.div [ css [ Css.height (Css.pct 100) ]]
-  [ global 
-    [ class "repl"
-      [ fontFamily monospace
-      , fontSize (Css.px 14)
-      , margin (Css.px 0)
-      , Css.color theme.text
-      , Css.backgroundColor theme.primary
-      , Css.focus 
-        [ outline zero
-        ]
-      ]
-    ]
-  , Html.Styled.div [ 
-    css [ Css.height headerHeight
-          , Css.width (Css.vw 100)
-          , Css.float left
-        ]
-    ] 
-    [ mainHeader []
-    ]
-  , Html.Styled.div [ 
-    css [ Css.height replHeight
-          , Css.width (Css.vw 50)
-          , Css.float left
-        ]
-    ] 
-    [ mainInput [] model.mainCode
-    ]
-  , Html.Styled.div [ 
-    css [ Css.height replHeight
-          , Css.width (Css.vw 50)
-          , Css.float left
-        ]
-    ] 
-    [ mainOutput [] model.codeOutput
-    ]
-  ]
+    let
+        headerHeight =
+            Css.vh 10
 
-mainHeader : List (Css.Style) -> Html.Styled.Html Msg
+        replHeight =
+            Css.calc (Css.vh 100) Css.minus headerHeight
+    in
+    Html.Styled.div [ css [ Css.height (Css.pct 100) ] ]
+        [ global
+            [ class "repl"
+                [ fontFamily monospace
+                , fontSize (Css.px 14)
+                , margin (Css.px 0)
+                , Css.color theme.text
+                , Css.backgroundColor theme.primary
+                , Css.focus
+                    [ outline zero
+                    ]
+                ]
+            ]
+        , Html.Styled.div
+            [ css
+                [ Css.height headerHeight
+                , Css.width (Css.vw 100)
+                , Css.float left
+                ]
+            ]
+            [ mainHeader []
+            ]
+        , Html.Styled.div
+            [ css
+                [ Css.height replHeight
+                , Css.width (Css.vw 50)
+                , Css.float left
+                ]
+            ]
+            [ mainInput [] model.mainCode
+            ]
+        , Html.Styled.div
+            [ css
+                [ Css.height replHeight
+                , Css.width (Css.vw 50)
+                , Css.float left
+                ]
+            ]
+            [ mainOutput [] model.codeOutput
+            ]
+        ]
+
+
+mainHeader : List Css.Style -> Html.Styled.Html Msg
 mainHeader attrs =
-  let
-    borderSize = (Css.px 0.25)
-    heightSize = CssUtils.calcDimension (Css.pct 100) (Css.px 0) borderSize
-  in
-  Html.Styled.div [
-    css ([ Css.backgroundColor theme.secondary 
-         , Css.minHeight heightSize
-         , border (Css.px 0)
-         , borderBottom3 borderSize Css.solid theme.border
-         ] ++ attrs)
-    ]
-    [
-      Html.Styled.button 
-        [ Html.Styled.Events.onClick SendPost
-        ]
-        [ Html.Styled.text "Run" ]
-    ]
+    let
+        borderSize =
+            Css.px 0.25
 
-mainInput : List (Css.Style) ->  String -> Html.Styled.Html Msg
-mainInput attrs code = 
-  let
-    paddingSize = Css.px 2
-    borderSize = Css.px 0.125
-    widthSize = CssUtils.calcDimension (Css.pct 100) paddingSize borderSize
-    heightSize = CssUtils.calcDimension (Css.pct 100) paddingSize (Css.px 0)
-  in
-  Html.Styled.textarea 
-    [ Html.Styled.Events.onInput TextUpdate
-    , onTab TabDown
-    , Html.Styled.Attributes.value code 
-    , Html.Styled.Attributes.autocomplete False
-    , Html.Styled.Attributes.spellcheck False
-    , Html.Styled.Attributes.classList [("repl", True)]
-    , css 
-      ([ Css.width widthSize
-       , Css.height heightSize
-       , Css.padding paddingSize
-       , Css.resize Css.none
-       , border (Css.px 0)
-       , borderRight3 borderSize Css.solid theme.border
-       ] ++ attrs)
-  ] []
+        heightSize =
+            CssUtils.calcDimension (Css.pct 100) (Css.px 0) borderSize
+    in
+    Html.Styled.div
+        [ css
+            ([ Css.backgroundColor theme.secondary
+             , Css.minHeight heightSize
+             , border (Css.px 0)
+             , borderBottom3 borderSize Css.solid theme.border
+             ]
+                ++ attrs
+            )
+        ]
+        [ Html.Styled.button
+            [ Html.Styled.Events.onClick SendPost
+            ]
+            [ Html.Styled.text "Run" ]
+        ]
+
+
+mainInput : List Css.Style -> String -> Html.Styled.Html Msg
+mainInput attrs code =
+    let
+        paddingSize =
+            Css.px 2
+
+        borderSize =
+            Css.px 0.125
+
+        widthSize =
+            CssUtils.calcDimension (Css.pct 100) paddingSize borderSize
+
+        heightSize =
+            CssUtils.calcDimension (Css.pct 100) paddingSize (Css.px 0)
+    in
+    Html.Styled.textarea
+        [ Html.Styled.Events.onInput TextUpdate
+        , onTab TabDown
+        , Html.Styled.Attributes.value code
+        , Html.Styled.Attributes.autocomplete False
+        , Html.Styled.Attributes.spellcheck False
+        , Html.Styled.Attributes.classList [ ( "repl", True ) ]
+        , css
+            ([ Css.width widthSize
+             , Css.height heightSize
+             , Css.padding paddingSize
+             , Css.resize Css.none
+             , border (Css.px 0)
+             , borderRight3 borderSize Css.solid theme.border
+             ]
+                ++ attrs
+            )
+        ]
+        []
 
 
 onTab : msg -> Html.Styled.Attribute msg
 onTab msg =
-  let _ = Debug.log "onTab" Html.Events.keyCode
-  in
-  let
-    isTabKey keyCode =
-      if keyCode == 9 then
-        Decode.succeed msg
-      else
-        Decode.fail "silent failure :)"
-  in
-  Html.Events.keyCode
-  |> Decode.andThen isTabKey
-  |> Decode.map (\x -> { message = x, stopPropagation = True, preventDefault = True })
-  |> Html.Styled.Events.custom "keydown"
+    let
+        _ =
+            Debug.log "onTab" Html.Events.keyCode
+    in
+    let
+        isTabKey keyCode =
+            if keyCode == 9 then
+                Decode.succeed msg
 
-mainOutput : List (Css.Style) -> String -> Html.Styled.Html Msg
-mainOutput attrs output = 
-  let
-    paddingSize = Css.px 2
-    borderSize = Css.px 0.125
-    widthSize = CssUtils.calcDimension (Css.pct 100) paddingSize borderSize
-    heightSize = CssUtils.calcDimension (Css.pct 100) paddingSize (Css.px 0)
-  in
-  Html.Styled.p
-    [ Html.Styled.Attributes.classList [("repl", True)]
-    , css
-      ([ Css.width widthSize
-       , Css.height heightSize
-       , Css.padding paddingSize
-       , border (Css.px 0)
-       , borderLeft3 borderSize Css.solid theme.border
-       ] ++ attrs)
-    ]
-    [Html.Styled.text output]
+            else
+                Decode.fail "silent failure :)"
+    in
+    Html.Events.keyCode
+        |> Decode.andThen isTabKey
+        |> Decode.map (\x -> { message = x, stopPropagation = True, preventDefault = True })
+        |> Html.Styled.Events.custom "keydown"
+
+
+mainOutput : List Css.Style -> String -> Html.Styled.Html Msg
+mainOutput attrs output =
+    let
+        paddingSize =
+            Css.px 2
+
+        borderSize =
+            Css.px 0.125
+
+        widthSize =
+            CssUtils.calcDimension (Css.pct 100) paddingSize borderSize
+
+        heightSize =
+            CssUtils.calcDimension (Css.pct 100) paddingSize (Css.px 0)
+    in
+    Html.Styled.p
+        [ Html.Styled.Attributes.classList [ ( "repl", True ) ]
+        , css
+            ([ Css.width widthSize
+             , Css.height heightSize
+             , Css.padding paddingSize
+             , border (Css.px 0)
+             , borderLeft3 borderSize Css.solid theme.border
+             ]
+                ++ attrs
+            )
+        ]
+        [ Html.Styled.text output ]
